6595763fb87c0f218d9c3562ed94838253b20c34
Nicola Prete <nicola.prete@mat.tn.it>
2026-03-05 15:56:36 +0000

    feat: handler da modelandview

---
+++ hello-world/src/main/java/it/mat/hello_world/controllers/ModelController.java
@@ -0,0 +1,15 @@
+package it.mat.hello_world.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("other")
+public class ModelController {
+      @GetMapping("fixed")
+        public ModelAndView handlerFixed() {
+        return new ModelAndView("hello-world-model").addObject("maker", "A.Trentini");
+    }
+}   
